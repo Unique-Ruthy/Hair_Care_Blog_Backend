0a34661c7a0d072e82ef59d1280364b25fe1fe8d
Amarachi <uniqueruthamara@gmail.com>
2022-11-11 17:02:36 +0100

    modified index

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 //loads data from env variable
 require("dotenv").config();
 const authRoute = require("./routes/auth");
